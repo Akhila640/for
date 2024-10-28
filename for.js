5a72c2e5a2cde12d94e55bff853d9bf8597a6bf9
Akhila640 <akhilasiddi00@gmail.com>
2024-10-28 11:48:27 +0530

    Add files via upload

---
+++ for.js
@@ -0,0 +1,41 @@
+// Switch case
+switch("Friday"){
+    case "Monday":
+        console.log("it is monday")
+        break;
+    case "Tuesday":
+        console.log("it is tuesday")
+        break;
+    case "Wednesday":
+        console.log("it is Wednesday")
+        break;
+    case "Thursday":
+        console,log("it is Thursday")
+        break;
+    case "Friday":
+        console.log("it is Friday")
+        break;
+    case "Saturday":
+        console.log("it is Saturday")
+        break;
+    case "Sunday":
+        console.log("it is Sunday")
+        break;
+    default:
+        console.log("pls choose correct day")                            
+}
+
+
+
+
+
+// for of loop
+ var a=[1,2,3,4,5]
+ for(i=1;i<=a.length;i++)
+    console.log(i)
+
+
+var b="Akhila";
+for(var char of b){
+    console.log(char)
+}
